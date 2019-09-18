baf556efdba3dd58fe90f3f973f4725c2297ec0b
1298743328szyasy <1298743328@qq.com>
2019-09-18 20:29:26 +0800

    Initial commit

---
+++ Day05/he.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//class ArrayExample
+//{
+//    static void Main() 
+//    {
+//    }
+//    public static int Sum(int[] values)
+//    {
+//        int sum = 0;
+//        foreach (int item in values)
+//        {
+//            
+//        }
+//        return sum;
+//    }
+//}
---
+++ Day05/jiecheng.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day05
+{
+    public class jiecheng
+    {
+        public static void Main()
+        {
+            string[] str = {"aa", "333", "ccc"};
+            string seperator = "|";
+            Console.WriteLine(Join(str,"|"));
+        }
+
+        public static string Join(string[] strs,string seperator)
+        {
+            string a="";
+            for (int i = 0; i < strs.Length; i++)
+            {
+                a = a + strs[i] + (strs.Length-1==i?"":seperator);
+            }
+            return a;
+        }
+    }
+}
